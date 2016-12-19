543bfdcc2d5cf65e4d42f12826813cfe327cf475
Meredith Moore <meredithmoore@Merediths-MacBook-Pro.local>
2016-12-18 23:19:23 -0500

    F--- YEAH IT WORKS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -25,61 +25,95 @@
    //   To that end, do not refresh the page as a means to restart the game.
 
    // * The random number shown at the start of the game should be between 19 - 120.
-    
+
    // * Each crystal should have a random hidden value between 1 - 12.
+   $(document).ready(function() {
+       // variable to store randomly-generated Number to Match
+       var numToMatch;
 
-$(document).ready(function() {
-    // variable to store randomly-generated Number to Match
-    var numToMatch;
+       var crystalValue;
 
-    // variable to store random number assigned to each jewel
-    var jewel1Value;
-    var jewel2Value;
-    var jewel3Value;
-    var jewel4Value;
+       // variable to store random number assigned to each jewel
+       var jewel1Value;
+       var jewel2Value;
+       var jewel3Value;
+       var jewel4Value;
 
-    // variable to store accumulating total
-    var runningTotal;
+       // variable to store accumulating total
+       var runningTotal;
 
-    // variable to store number of user wins
-    var numWins;
+       // variable to store number of user wins
+       var numWins = 0;
+       $("#wins").text(numWins);
 
-    // variable to store number of user losses
-    var numLosses;
-
-
-    // call function to reset the game
-    function reset() {
-        
-        // generate a random number to match
-        numToMatch = Math.floor((Math.random() * 120) + 19);
-
-        // generate a random value for each jewel
-        jewel1Value = Math.floor((Math.random() * 12) + 1);
-        jewel2Value = Math.floor((Math.random() * 12) + 1);
-        jewel3Value = Math.floor((Math.random() * 12) + 1);
-        jewel4Value = Math.floor((Math.random() * 12) + 1);
-
-        $("#wins").text(numLosses);
-        $("#losses").text(numWins);
-        $("#number-to-match").text(numToMatch);
-        $("score").append(runningTotal)
+       // variable to store number of user losses
+       var numLosses = 0;
+       $("#losses").text(numLosses);
 
 
+       // function to reset the game
+       function reset() {
+           runningTotal = 0
+               // generate a random number to match
+           numToMatch = Math.floor((Math.random() * 120) + 19);
 
-    }
-    
-numWins = 4;
-numLosses = 9;
-numToMatch = 20;
-runningTotal = 5;
-$("#score").append(runningTotal)
-console.log(runningTotal)
+           // generate a random value for each jewel
+           jewel1Value = Math.floor((Math.random() * 12) + 1);
+           jewel2Value = Math.floor((Math.random() * 12) + 1);
+           jewel3Value = Math.floor((Math.random() * 12) + 1);
+           jewel4Value = Math.floor((Math.random() * 12) + 1);
 
-$("#wins").text(numLosses);
-$("#losses").text(numWins);
-$("#number-to-match").text(numToMatch);
+           // write the values to each panel/well
+           $("#score").text("Your Current Score:")
+           $("#number-to-match").text(numToMatch);
 
-});
+           console.log("jewel1Value: " + jewel1Value);
+           console.log("jewel2Value: " + jewel2Value);
+           console.log("jewel3Value: " + jewel3Value);
+           console.log("jewel4Value: " + jewel4Value);
+
+       }
+
+       function checks() {
+           $("#score").text("Your Current Score: " + runningTotal);
+           if (runningTotal === numToMatch) {
+               alert("You win!");
+               numWins++;
+               $("#wins").text(numWins);
+               reset();
+           } else if (runningTotal >= numToMatch) {
+               alert("You lose!!");
+               numLosses++;
+               $("#losses").text(numLosses);
+               reset();
+           } else {
+               return
+           }
+       }
+
+       // call function to reset game
+       reset()
 
 
+       $("#number-to-match").text(numToMatch);
+
+       $("#crystal-1").click(function() {
+           runningTotal = jewel1Value + runningTotal;
+           checks();
+       });
+
+       $("#crystal-2").click(function() {
+           runningTotal = jewel2Value + runningTotal;
+           checks();
+       });
+
+       $("#crystal-3").click(function() {
+           runningTotal = jewel3Value + runningTotal;
+           checks();
+       });
+
+       $("#crystal-4").click(function() {
+           runningTotal = jewel4Value + runningTotal;
+           checks();
+       });
+   });
